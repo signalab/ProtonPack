be0ff95ace233c43225f785aea0a74e3646dd458
NateraLuis <nateraluis@gmail.com>
2017-06-13 16:12:51 -0500

    Plot probability

--- Distribution.py
+++ Distribution.py
@@ -1,51 +1,37 @@
 import matplotlib.pyplot as plt
-from numpy.random import normal
 import pandas as pd
-import io
-
-# data = pd.read_csv('Accounts_Bots_Gasolinazo.csv')
-# x = data.Score
-# x = [account for account in x if account != "Null"]
-#
-# print (x)
-# plt.hist(x)
-# plt.title("Gaussian Histogram")
-# plt.xlabel("Value")
-# plt.ylabel("Frequency")
-# plt.show()
 
 
-df = pd.read_csv('Accounts_Bots_Gasolinazo.csv', sep=',')
-x = df
-x = [account for account in x if account != 'Null']
-plt.hist(x['Score'], bins=30)
+def getScoresFrequency():
+    df = pd.read_csv(input('Please insert the name of the csv file: ')) #df = DataFrame
+    scoresColumn = df.Score #scoresColumn = Series
+    scoreCountSet = set() # set of tuples
+    #Fill the set
+    for score in scoresColumn:
+        if score != 'Null':
+            scoreCountSet.add((score,1))
+    #Check frequency for each score
+    for score in scoresColumn:
+        for tup in scoreCountSet:
+            if tup[0] == score:
+                scoreValue = tup[1]
+                scoreCountSet.remove(tup)
+                newTup = (score, scoreValue + 1);
+                scoreCountSet.add(newTup)
+                break
+    return scoreCountSet
+
+def plotScores():
+    scoresData = getScoresFrequency()
+    plt.scatter(*zip(*scoresData))
+    plt.title('Accounts distribution for bot probability\n')
+    plt.xlabel("Bot Probability")
+    plt.ylabel("Accounts")
+    plt.show()
 
 
-#
-# import matplotlib.pyplot as plt
-# import pandas as pd
-#
-# data = pd.read_csv('Accounts_Bots_Gasolinazo.csv', sep=',',header='Score', index_col =1)
-# x = data
-# x = [account for account in x if account != "Null"]
-# data.plot(kind='bar')
-# plt.ylabel('Frequency')
-# plt.xlabel('Words')
-# plt.title('Title')
-#
-# plt.show()
-#
-# import numpy as np
-# import matplotlib.mlab as mlab
-# import matplotlib.pyplot as plt
-# import pandas as pd
-#
-#
-# import matplotlib.pyplot as plt
-# import numpy as np
-# df = pd.read_csv('Accounts_Bots_Gasolinazo.csv')
-# x = df.Score
-# x = [account for account in x if account != "Null"]
-# x = np.fromstring(x)
-# print (x)
-# plt.hist(x)
+def main():
+    plotScores()
+
+if __name__ == '__main__':
+    main()
